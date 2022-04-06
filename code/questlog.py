b9086b47efae9ad95b1ac4171320669e563e72eb
Bastien Gorissen <kadomony@gmail.com>
2022-04-06 12:09:06 +0200

    Add a new quest

--- code/questlog.py
+++ code/questlog.py
@@ -2,7 +2,8 @@ quests = [
     "Understand Git",
     "Mend the Elden Ring",
     "Find a new quest idea",
-    "Destroy the One Ring"
+    "Destroy the One Ring",
+    "Save everyone"
 ]
 
 for quest in quests:
